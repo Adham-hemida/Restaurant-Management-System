e7dbd616c27144aa8c81e7b58c38c37017f6d7f5
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-08-21 14:32:40 +0300

    implement refresh token with CQRS

---
+++ RestaurantProject.Application/Features/Authentication/Commands/Handlers/GenerateRefreshTokenCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantProject.Application.Features.Authentication.Commands.Handlers;
+
+public class GenerateRefreshTokenCommandHandler(IAuthService authService) : IRequestHandler<GenerateRefreshTokenCommand, Result<AuthResponse>>
+{
+	private readonly IAuthService _authService = authService;
+
+	public async Task<Result<AuthResponse>> Handle(GenerateRefreshTokenCommand request, CancellationToken cancellationToken)
+	{
+		return await _authService.GetRefreshTokenAsync(request.RefreshTokenRequest, cancellationToken);
+	}
+}
---
+++ RestaurantProject.Application/Features/Authentication/Commands/Validators/GenerateRefreshTokenCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantProject.Application.Features.Authentication.Commands.Validators;
+public class GenerateRefreshTokenCommandValidator : AbstractValidator<GenerateRefreshTokenCommand>
+{
+	public GenerateRefreshTokenCommandValidator()
+	{
+		RuleFor(x => x.RefreshTokenRequest)
+			.NotNull()
+			.WithMessage("Refresh token request cannot be null.")
+			.SetValidator(new RefreshTokenRequestValidator());
+	}
+}
