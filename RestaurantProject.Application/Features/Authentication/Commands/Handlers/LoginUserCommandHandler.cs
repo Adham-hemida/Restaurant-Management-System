1e97d527c8c510707395e3cb6c7fb1876e9bfb7c
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-08-21 14:06:12 +0300

    Implement Login with CQRS pattern - Added LoginUserCommand & Validator - Added LoginUserCommandHandler with JWT generation - Added AuthResponse model - Integrated FluentValidation for LoginRequest

---
+++ RestaurantProject.Application/Features/Authentication/Commands/Handlers/LoginUserCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using RestaurantProject.Application.Abstractions;
+using RestaurantProject.Application.Contracts.Authentication;
+using RestaurantProject.Application.Features.Authentication.Commands.Models;
+using RestaurantProject.Application.Interfaces.IAuthentication;
+
+namespace RestaurantProject.Application.Features.Authentication.Commands.Handlers;
+
+public class LoginUserCommandHandler(IAuthService authService) : IRequestHandler<LoginUserCommand, Result<AuthResponse>>
+{
+	private readonly IAuthService _authService = authService;
+
+	public async Task<Result<AuthResponse>> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+	{
+          return await _authService.GetTokenAsync(request.LoginRequest, cancellationToken);
+	}
+}
+
---
+++ RestaurantProject.Application/Features/Authentication/Commands/Validators/LoginUserCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using RestaurantProject.Application.Contracts.Authentication;
+using RestaurantProject.Application.Features.Authentication.Commands.Models;
+
+namespace RestaurantProject.Application.Features.Authentication.Commands.Validators;
+public class LoginUserCommandValidator : AbstractValidator<LoginUserCommand>
+{
+	public LoginUserCommandValidator()
+	{
+		RuleFor(x => x.LoginRequest)
+				.NotNull()
+				.WithMessage("Login request cannot be null.")
+				.SetValidator(new LoginRequestValidator());
+
+
+	}
+}
