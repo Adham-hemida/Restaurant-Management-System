5f47e26a20717079466928cab4ec89e41360e20b
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-08-25 14:10:25 +0300

    Added Check for menuItem Before Upload Photo

--- Restaurant.Infrastructure/Implementaion/Services/FileService.cs
+++ Restaurant.Infrastructure/Implementaion/Services/FileService.cs
@@ -2,17 +2,25 @@
 using Microsoft.AspNetCore.Http;
 using RestaurantProject.Application.Abstractions;
 using RestaurantProject.Application.Contracts.Photo;
+using RestaurantProject.Application.ErrorHandler;
 using RestaurantProject.Application.Interfaces.IService;
 using RestaurantProject.Domain.Entites;
 
 namespace RestaurantProject.Infrastructure.Implementaion.Services;
-public class FileService (IWebHostEnvironment webHostEnvironment,IUploadedFileRepository uploadedFileRepository) : IFileService
+public class FileService (IWebHostEnvironment webHostEnvironment,
+	IUploadedFileRepository uploadedFileRepository,
+	IMenuItemRepository menuItemRepository) : IFileService
 {
 	private readonly string _imagePath = $"{webHostEnvironment.WebRootPath}/images";
 	private readonly IUploadedFileRepository _uploadedFileRepository = uploadedFileRepository;
+	private readonly IMenuItemRepository _menuItemRepository = menuItemRepository;
 
 	public async Task<Result> UploadImageAsync(int menuItemId, UploadImageRequest request, CancellationToken cancellationToken = default)
 	{
+		var menuItemIsExist = await _menuItemRepository.GetAsQueryable().AnyAsync(x => x.Id == menuItemId && x.IsActive, cancellationToken);
+		if (!menuItemIsExist)
+			return Result.Failure(MenuItemErrors.MenuItemNotFound);
+
 		var path = Path.Combine(_imagePath, request.Image.FileName);
 
 		using var stream = File.Create(path);
