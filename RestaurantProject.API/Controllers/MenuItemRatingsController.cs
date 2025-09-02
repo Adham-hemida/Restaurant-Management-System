a5613000d5b7039caf05871078d72ba91d990d63
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-09-02 14:37:20 +0300

    Updated  AddMenuItemRating Endpoint

--- RestaurantProject.API/Controllers/MenuItemRatingsController.cs
+++ RestaurantProject.API/Controllers/MenuItemRatingsController.cs
@@ -7,6 +7,7 @@ using RestaurantProject.Application.Features.MenuCategory.Command.Models;
 using RestaurantProject.Application.Features.MenuItem.Queries.Models;
 using RestaurantProject.Application.Features.MenuItemRating.Commands.Models;
 using RestaurantProject.Application.Features.MenuItemRating.Queries.Models;
+using RestaurantProject.Domain.Entites;
 
 namespace RestaurantProject.API.Controllers;
 [Route("api/Order/{orderId}/MenuItem/{menuItemId}/[controller]")]
@@ -28,7 +29,7 @@ public class MenuItemRatingsController(IMediator mediator) : ControllerBase
 	public async Task<IActionResult> Create([FromRoute] int orderId, [FromRoute] int menuItemId, [FromBody] MenuItemRatingRequest request, CancellationToken cancellationToken)
 	{
 		var result = await _mediator.Send(new AddMenuItemRatingCommand(orderId, menuItemId, request), cancellationToken);
-		return result.IsSuccess ? Ok(result.Value): result.ToProblem();
+		return result.IsSuccess ?CreatedAtAction(nameof(GetById),new { orderId , menuItemId , menuItemRatingId =result.Value.Id},result.Value): result.ToProblem();
 	}
 
 	[HttpPut("{menuItemRatingId}/toggleStatus")]
