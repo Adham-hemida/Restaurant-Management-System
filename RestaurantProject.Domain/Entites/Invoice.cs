154aee1b7a8a329a4e878d5b75f74deb04074ecc
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-09-09 12:02:51 +0300

    Add Tax AndS erviceCharge To Invoice

--- RestaurantProject.Domain/Entites/Invoice.cs
+++ RestaurantProject.Domain/Entites/Invoice.cs
@@ -7,6 +7,10 @@ public class Invoice : AuditableEntity
 	public DateTime? PaidAt { get; set; }
 	public bool IsPaid { get; set; } = false;
 	public bool IsActive { get; set; } = true;
+	public decimal Tax { get; set; } = 0;
+	public decimal ServiceCharge { get; set; } = 0;
+
+	public decimal FinalAmount => TotalAmount + Tax + ServiceCharge;
 
 	public int OrderId { get; set; }
 	public Order Order { get; set; } = default!;
